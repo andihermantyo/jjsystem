152de7740dbe36945f46acf9f15f79d1d3437980
Stephanus Andi Hermantyo <andihermantyo@gmail.com>
2024-11-11 02:10:38 +0700

    chore: configure multiple app variants

---
+++ app.config.js
@@ -0,0 +1,39 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const APP_NAME = 'JJSystem';
+const ANDROID_PACKAGE = 'com.andihermantyo.jjsystem';
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return `${APP_NAME} (Dev)`;
+  }
+
+  if (IS_PREVIEW) {
+    return `${APP_NAME} (Prev)`;
+  }
+
+  return `${APP_NAME}: JJ Poultry Petshop`;
+};
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return `${ANDROID_PACKAGE}.dev`;
+  }
+
+  if (IS_PREVIEW) {
+    return `${ANDROID_PACKAGE}.prev`;
+  }
+
+  return ANDROID_PACKAGE;
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  slug: 'jjsystem',
+  description: 'JJ Poultry Petshop Information System',
+  orientation: 'default',
+  android: {
+    package: getUniqueIdentifier(),
+  },
+});
