ffa4588656cc93f0c5160f268a89ee7fa475753f
Stephanus Andi Hermantyo <andihermantyo@gmail.com>
2024-11-16 18:11:27 +0700

    chore: update '.eslintrc.js'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
+  ignorePatterns: ['/dist/*'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
