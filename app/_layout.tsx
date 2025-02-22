f08003bb097068a1d6523bf5da86b4d3deb6b504
Stephanus Andi Hermantyo <andihermantyo@gmail.com>
2025-02-22 22:13:37 +0700

    feat: update the UI main screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
+import { useKeepAwake } from 'expo-keep-awake';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
+  useKeepAwake();
+
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,7 @@
-import { Text, View } from 'react-native';
+import * as Application from 'expo-application';
+import Constants from 'expo-constants';
+import { Platform, StyleSheet, View } from 'react-native';
+import { Text, Title } from 'react-native-paper';
 
 export default function Index() {
   return (
@@ -9,7 +12,41 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Title style={styles.title}>
+        {Platform.OS === 'android' ? Application.applicationName : 'JJSystem'}
+      </Title>
+      <Text style={(styles.paragraph, styles.title)}>
+        {process.env.APP_VARIANT}
+      </Text>
+      <Text style={(styles.paragraph, styles.title)}>
+        {Platform.OS === 'android' &&
+          `Versi ${Application.nativeApplicationVersion} (${Application.nativeBuildVersion})`}
+      </Text>
+      <Text style={(styles.paragraph, styles.title)}>
+        Informasi perangkat : {Constants.deviceName}{' '}
+        {Platform.OS === 'android' &&
+          (Constants.platform?.android
+            ? `(Android ${Constants.systemVersion})`
+            : '')}
+      </Text>
+      <Text style={(styles.paragraph, styles.title)}>
+        Informasi server : {process.env.EXPO_PUBLIC_SUPABASE_URL}
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+  },
+  button: {
+    marginBottom: 10,
+  },
+  paragraph: {
+    marginBottom: 10,
+  },
+  divider: {
+    marginVertical: 20,
+  },
+});
